b7853844abc56d18a64850d7570ebaafb0b104bd
annapolec <ania.polec@gmail.com>
2015-12-07 20:08:17 +0100

    Calculating weather condition scale form values

--- app/models/danger_scale.rb
+++ app/models/danger_scale.rb
@@ -2,11 +2,11 @@ class DangerScale < ActiveRecord::Base
 	#belongs_to :track
 	#ogolnie dla calej Babiej Gory
 	def DangerScale.calculate_danger_scale
-		temperature = WeatherCondition.last.temperature
-		rain = WeatherCondition.last.rain
-		wind = WeatherCondition.last.wind
+		temperature_rate = WeatherCondition.last.temperature_rate
+		rain_rate = WeatherCondition.last.rain_rate
+		wind_rate = WeatherCondition.last.wind_rate
 		avalanche_danger_level = WeatherCondition.last.avalanche_danger_level
 
-		danger_scale = (temperature + rain + wind + avalanche_danger_level)/4
+		danger_scale = (temperature_rate + rain_rate + wind_rate + avalanche_danger_level)/4
 	end
 end
--- app/models/weather_condition.rb
+++ app/models/weather_condition.rb
@@ -1,3 +1,21 @@
 class WeatherCondition < ActiveRecord::Base
 	#belongs_to :track
+	def temperature_rate
+	return 1 if (self.temperature >=15 and temperature <=24) or (self.temperature <=5 and self.temperature >=-3)
+	return 2 if (self.temperature >24 and self.temperature <=30) or (self.temperature <-3 and self.temperature >=-15)
+	return 3 if (self.temperature >30) or (self.temperature < -15)
+	end
+
+	def rain_rate
+	return 0 if self.rain < 15
+	return 1 if self.rain >=15 and self.rain <=40
+	return 2 if self.rain > 40 and self.rain <= 70
+	return 3 if self.rain >70
+	end
+
+	def wind_rate
+	return 1 if self.wind <= 30 
+	return 2 if self.wind > 30 and self.wind <= 80
+	return 3 if self.wind >80
+	end
 end
